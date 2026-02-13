1661ae93399fc9ad43b251215750fb687a0e66a1
WangQing <2917021186@qq.com>
2026-02-13 08:47:27 +0000

    [ascend] refactor qwen3next

--- dlinfer/framework/lmdeploy_ext/device/__init__.py
+++ dlinfer/framework/lmdeploy_ext/device/__init__.py
@@ -102,6 +102,122 @@ def patch_async_sampling_logits():
     BaseModelAgent.async_sampling_logits = async_sampling_logits
 
 
+##### patch cache engine #####
+def patch_contiguous_cache_engine():
+    from lmdeploy.pytorch.config import CacheConfig, ModelConfig
+    from functools import reduce
+    from math import gcd
+    from lmdeploy.pytorch.engine import cache_engine
+
+    @classmethod
+    def _cache_engine_allocate_caches(
+        cls,
+        num_blocks: int,
+        model_config: ModelConfig,
+        cache_config: CacheConfig,
+        world_size: int,
+        device: str,
+    ):
+        """Allocate caches."""
+        num_layers = model_config.num_layers
+
+        # get all descs
+        k_cache_desc = cls.get_k_cache_desc(model_config, cache_config, world_size)
+        v_cache_desc = cls.get_v_cache_desc(model_config, cache_config, world_size)
+        quant_cache_descs = cls.get_quant_cache_descs(
+            k_cache_desc, v_cache_desc, model_config, cache_config
+        )
+        custom_cache_descs = cls.get_custom_cache_descs(model_config, cache_config)
+        cache_descs = (
+            [k_cache_desc, v_cache_desc] + quant_cache_descs + custom_cache_descs
+        )
+
+        # get mempool size
+        mem_pool_size = 0
+        alignments = []
+        for desc in cache_descs:
+            mem_pool_size += desc.aligned_size
+            alignments.append(desc.alignment)
+
+        # compute gcd of alignments
+        alignments_gcd = reduce(gcd, alignments) if alignments else 1
+        assert (
+            mem_pool_size % alignments_gcd == 0
+        ), "mem_pool_size must be divisible by alignments_gcd"
+
+        # create pool
+        mem_pool = torch.zeros(
+            (mem_pool_size // alignments_gcd, num_layers, num_blocks, alignments_gcd),
+            dtype=torch.uint8,
+            device=device,
+        )
+
+        # slice caches
+        caches = []
+        remain_pool = mem_pool
+        for desc in cache_descs:
+            cache = (
+                remain_pool[: desc.size // alignments_gcd, :, :, :]
+                .view(desc.dtype)
+                .view((num_layers, num_blocks, *desc.shape))
+            )
+            remain_pool = remain_pool[desc.aligned_size // alignments_gcd :, :, :, :]
+            caches.append(cache)
+        return mem_pool, caches
+
+    cache_engine.CacheEngine.allocate_caches = _cache_engine_allocate_caches
+
+
+##### patch state cache engine #####
+def patch_state_cache_engine():
+    from typing import List, Tuple
+    from lmdeploy.pytorch.engine import cache_engine
+    from lmdeploy.pytorch.engine.cache_engine import CacheDesc
+
+    @staticmethod
+    def _state_cache_engine_allocate_caches(num_caches: int, state_shapes: List[Tuple[Tuple[int], torch.dtype]], device: torch.device):
+        """Allocate cache implement."""
+
+        # only support [DT_FLOAT,DT_INT32,DT_INT64,DT_FLOAT16,DT_INT8,DT_BOOL,DT_BFLOAT16,]
+        cache_dtype = torch.int8
+        if len(state_shapes) == 0 or num_caches == 0:
+            return torch.empty((0, 0), dtype=cache_dtype, device=device), []
+
+        # Ascend kernel causal_comv1d_update_npu requires the shape of conv_cache to be (B, K, D) and continuous in the K dimension
+        cache_descs = []
+        for shape, dtype in state_shapes:
+            if len(shape) == 3:
+                cache_descs.append(CacheDesc((shape[0], shape[2], shape[1]), dtype))
+            else:
+                cache_descs.append(CacheDesc(shape, dtype))
+
+        # get mempool size
+        mem_pool_size = 0
+        for desc in cache_descs:
+            mem_pool_size += desc.aligned_size
+
+        # create pool
+        mem_pool = torch.zeros((num_caches, mem_pool_size), dtype=cache_dtype, device=device)
+
+        # slice caches
+        caches = []
+        remain_pool = mem_pool
+        for desc in cache_descs:
+            cache = remain_pool[:, :desc.size].view(desc.dtype).view((num_caches, *desc.shape))
+            remain_pool = remain_pool[:, desc.aligned_size:]
+            caches.append(cache)
+        return mem_pool, caches
+
+    cache_engine.StateCacheEngine.allocate_caches = _state_cache_engine_allocate_caches
+
+
+def patch_qwen3_next():
+    from lmdeploy.pytorch.models import module_map
+    module_map.DEVICE_SPECIAL_MODULE_MAP['ascend'] = {
+        'Qwen3NextForCausalLM': 'dlinfer.framework.lmdeploy_ext.device.ascend_qwen3_next.Qwen3NextForCausalLM',
+    }
+
+
 @lru_cache(1)
 def import_vendor_module(vendor_name_str):
     if vendor_name_str in vendor:
@@ -112,6 +228,11 @@ def vendor_device_init():
     import_vendor_module(vendor_name)
     patch_compiled_func()
     patch_async_sampling_logits()
+    if vendor_name in ["camb", "ascend"]:
+        patch_contiguous_cache_engine()
+    if vendor_name == "ascend":
+        patch_state_cache_engine()
+        patch_qwen3_next()
 
 
 vendor_device_init()
